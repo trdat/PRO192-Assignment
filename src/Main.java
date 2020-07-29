a74180f4f9bb1e322bc2b116ec715d631e264eda
Dat Tran <trduydat@gmail.com>
2020-07-29 23:43:05 +0700

    Update class logic and format code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  *
@@ -10,7 +9,7 @@ public class Main {
 
     public static void displayMenu() {
         System.out.println("BOOK MANAGEMENT SYSTEM");
-        System.out.println("-------------------------------------");
+        System.out.println("-----------------------------------");
         System.out.println("1. Input and add book(s) to the end");
         System.out.println("2. Display all books");
         System.out.println("3. Search a book for given code");
@@ -26,11 +25,14 @@ public class Main {
         int userChoice;
         BookList book = new BookList();
 
+        // update the database without print out the message (false)
+        book.loadDataFromFile(false);
+        
         while (true) {
             // prompt user to get their choice
             displayMenu();
             userChoice = GetInput.Integer("Enter your choice", 0, 8);
-            book.loadDataFromFile();
+
             switch (userChoice) {
                 case 0:
                     // exit program
@@ -41,25 +43,32 @@ public class Main {
                     book.addOnes();
                     break;
                 case 2:
+                    // display all books
                     book.displayAllBooks();
                     break;
                 case 3:
+                    // search a book for given code.
                     book.searchByCode();
                     break;
                 case 4:
+                    // update the book’s price for given code.
                     book.updatePriceByCode();
                     break;
                 case 5:
-                    book.findMaxPrice();
+                    // find the (first) max price value.
+                    book.printMaxPriceValue();
                     break;
                 case 6:
+                    // sort the list ascendingly by code.
                     book.sortByCode();
                     break;
                 case 7:
+                    // remove the book having given code.
                     book.removeByCode();
                     break;
                 case 8:
-                    book.loadDataFromFile();
+                    // load data from file.
+                    book.loadDataFromFile(true);
                     break;
             }
             System.out.println();
